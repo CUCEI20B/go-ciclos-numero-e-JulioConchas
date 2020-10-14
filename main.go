db827b213077ba8318334d40e6401b31446751b2
jconchas <jconchas@ibm.com>
2020-10-14 18:14:51 -0500

    jconchas prueba 4

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import "fmt"
 func main()  {
 	var e float32
 	var infinito int
-	e = 1
+	e = 0
 
 	fmt.Scanln(&infinito)
 
-    for i := 0; i < infinito; i++ {
-        e = e + 1/float32(factorial(i));
+    for i := 0; i <= infinito; i++ {
+		if i == 0 {
+			e = e + 1
+		}else{
+			e = e + 1/float32(factorial(i));
+		}
     }
     fmt.Println("e = ",e)
 }
@@ -18,13 +22,10 @@ func factorial(n int) int{
     var f int;
     var i int;
 
-    if n == 0{
-        f = 1;
-    } else{
-        f = 1
-        for i = n; i >= 1; i-- {
-            f = f * i;
-        }
+    f = 1
+    for i = n; i >= 1; i-- {
+        f = f * i;
     }
+
     return f
 }
