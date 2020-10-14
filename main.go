5ebf97963d99a2c3308f325a557ce78b5843dae9
jconchas <jconchas@ibm.com>
2020-10-14 17:43:56 -0500

    jconchas prueba 01

--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var e float32
+    e = 0
+    for i := 0; i < 30; i++ {
+        e = e + 1/float32(factorial(i));
+    }
+    fmt.Println("e = ",e)
+}
+func factorial(n int) int{
+    var f int;
+    var i int;
+
+    if n == 0{
+        f = 1;
+    } else{
+        f = 1
+        for i = n; i >= 1; i-- {
+            f = f * i;
+        }
+    }
+    return f
 }
